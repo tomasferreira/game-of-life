814b940001103af04d6fa4d15f0e1801167b5f19
tomasferreira <tomasferreira@gmail.com>
2018-11-02 18:42:02 +0100

    added printer

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
 const chalk = require('chalk');
 const print = console.log;
+const Printer = require('./lib/Printer');
 
 const Grid = require('./lib/Grid');
 print(chalk.blue.bgYellow.bold('starting the game of life...'));
+
+let grid = new Grid(10, 10);
+grid.init();
+
+var printer = new Printer(grid);
+
+printer.printCoords(2,1);
+
+
+printer.printCell(grid.getCell(2,1));
+printer.printGrid();
