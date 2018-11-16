19007e12beed7db8ecfdd1ef2b32dfe8a337c8b9
tomasferreira <tomasferreira@gmail.com>
2018-11-17 00:35:12 +0100

    probably giving up on canvas

--- index.js
+++ index.js
@@ -33,4 +33,17 @@ function gameLoop() {
   }
 }
 
-var int = setInterval(gameLoop, interval);
+// var int = setInterval(gameLoop, interval);
+
+const createCanvas = require('canvas-prebuilt');
+const canvas = createCanvas(200, 200);
+const ctx = canvas.getContext('2d');
+ctx.font = '30px Impact';
+ctx.rotate(0.1);
+ctx.fillText('Awesome!', 50, 100);
+var text = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + text.width, 102);
+ctx.stroke();
