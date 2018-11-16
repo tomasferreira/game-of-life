87a5145895cd7bd699ebd1e933b0df2f2bf04be5
tomasferreira <tomasferreira@gmail.com>
2018-11-16 23:28:02 +0100

    finished refactoring

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 'use strict';
 const Printer = require('./lib/Printer');
 const Game = require('./lib/Game');
-const size = process.argv[2] ? parseInt(process.argv[2]) : 30;
-const numLiveCells = process.argv[3] ? parseInt(process.argv[3]) : 300;
-const interval = 0.1 * 1000;
+const w = process.argv[2] ? parseInt(process.argv[2]) : 30;
+const h = process.argv[3] ? parseInt(process.argv[3]) : 30;
+const numLiveCells = process.argv[4] ? parseInt(process.argv[4]) : 300;
+const interval = process.argv[5] ? parseInt(process.argv[5]) : 0.1 * 1000;
 
 var liveCells = [];
 
 for (let i = 0; i < numLiveCells; i++) {
   liveCells.push({
-    x: Math.floor(Math.random() * size),
-    y: Math.floor(Math.random() * size)
+    x: Math.floor(Math.random() * w),
+    y: Math.floor(Math.random() * h)
   });
 }
 
-let game = new Game(size, size);
+let game = new Game(w, h);
 var printer = new Printer(game);
-printer.printStart(size, numLiveCells);
+printer.printStart(h*w, numLiveCells);
 game.init(liveCells);
-
-let gameOver = false;
 
 function gameLoop() {
   printer.clear();
   printer.printStatus(game.cellNum, game.gen);
   printer.printGrid();
   game.calcNextGen();
-  gameOver = game.isGameOver();
   game.update();
 
-  if (gameOver) {
+  if (game.isGameOver) {
     clearInterval(int);
+    printer.printGameOver(game.gen);
   }
 }
 
